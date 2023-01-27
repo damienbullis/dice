d6ac27b16f5701a29f6be90c11715cf52c984a81
Damien <damien@bodhi.digital>
2023-01-26 23:06:11 -0600

    util type 1-100

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
-export const func = (a: number, b: number) => {
-  return a + b;
-};
+// create a utility type that will check that a number is between 1 and 100
+
+type Between1And100 = number & { __brand: "between1And100" };
+
+function between1And100(n: number): Between1And100 {
+  if (n < 1 || n > 100) {
+    throw new Error("Number is not between 1 and 100");
+  }
+  return n as Between1And100;
+}
