f0550ffbeda88b8cdc84ee734a5ea6be3067774a
Damien <damien@bodhi.digital>
2023-01-28 16:55:21 -0600

    mostly finishes dice and pool functions

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,2 @@
-// create a utility type that will check that a number is between 1 and 100
-
-type Between1And100 = number & { __brand: "between1And100" };
-
-function between1And100(n: number): Between1And100 {
-  if (n < 1 || n > 100) {
-    throw new Error("Number is not between 1 and 100");
-  }
-  return n as Between1And100;
-}
+export { default as Dice } from "./Dice";
+export { default as Pool } from "./Pool";
